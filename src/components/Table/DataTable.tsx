42d005e6a75546683d85e90daffc7b8c96dd0c6a
Mohammad Hossein Malek <mhosseinmalek@tract.eco>
2026-05-05 17:18:38 +0200

    feat(SCRUM-8): show risk score as a colour-coded progress bar

--- src/components/Table/DataTable.tsx
+++ src/components/Table/DataTable.tsx
@@ -1,6 +1,6 @@
 import { Spinner } from 'components/common/Spinner';
 import { StatusBadge } from 'components/common/Badge';
-import { RiskCell } from './RiskCell';
+import { RiskProgressBar } from 'components/common/RiskProgressBar';
 import { useSupplierList } from 'hooks/useSuppliers';
 import { useFilters } from 'hooks/useFilters';
 import { TableFilter } from './TableFilter';
@@ -49,7 +49,7 @@ export const DataTable = ({ onRowClick }: DataTableProps) => {
                 <td className="px-4 py-3 text-gray-600">{supplier.country}</td>
                 <td className="px-4 py-3 text-gray-600">{supplier.category}</td>
                 <td className="px-4 py-3">
-                  <RiskCell score={supplier.riskScore} />
+                  <RiskProgressBar score={supplier.riskScore} />
                 </td>
                 <td className="px-4 py-3">
                   <StatusBadge status={supplier.status} />
--- src/components/Table/RiskCell.tsx
+++
@@ -1,9 +0,0 @@
-import { RiskProgressBar } from '../common/RiskProgressBar';
-
-interface RiskCellProps {
-  score: number;
-}
-
-export const RiskCell = ({ score }: RiskCellProps) => {
-  return <RiskProgressBar score={score} />;
-};
